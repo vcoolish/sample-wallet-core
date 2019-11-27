de1a2e6520dadfc4ebb48e8f099fb4662abdaa63
Catenocrypt <catenocrypt@users.noreply.github.com>
2019-11-27 17:57:14 +0100

    Add detailed descriptions, disclaimer.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Sample [TrustWallet](https://www.trustwallet.com) [Wallet-Core](https://github.com/trustwallet/wallet-core) client application in C++
 
+## DISCLAIMER
+
+This is a sample application with demonstration purposes only, do not use it with real addresses, or real transactions, use it at your own risk.
+ 
 ## Prerequisites
 
 You need to download and build WalletCore yourself
@@ -26,3 +30,29 @@ Run it:
 
 ## What it Does
 
+The sample code should be self-documented, but here are some overview explanations:
+
+### Create a new wallet
+
+A brand new wallet, with a new mnemonic passphrase, is created with **HDWallet**.  The mnemonic is retrieved and printed.
+
+### Coin Type
+
+Many APIs are coin-agnostic, and require a coin type as input.
+In this sample we use Ethereum, whose value is *TWCoinType::TWCoinTypeEthereum*.
+
+### Derive the Ethereum address
+
+The multi-coin HDWallet implicitly contains addressess for each and every coin.
+To obtain our Eth address, we first retrieve the private key, then the address from the private key.
+Note that private keys should be always handled with uttermost care!
+
+### Sending Eth
+
+To send Eth from this address, the following steps are needed:
+
+* put together a send transaction (contains sender and receiver address, amount, etc.)
+* sign this transaction (using the private key)
+* broadcast the transaction to the P2P network (not done in this sample)
+
+TODO
--- sample.cpp
+++ sample.cpp
@@ -1,3 +1,5 @@
+#include <TrustWalletCore/TWAnyProto.h>
+#include <TrustWalletCore/TWAnySigner.h>
 #include <TrustWalletCore/TWCoinType.h>
 #include <TrustWalletCore/TWCoinTypeConfiguration.h>
 #include <TrustWalletCore/TWHDWallet.h>
@@ -10,8 +12,10 @@ using namespace std;
 
 int main()
 {
-    cout << "Wallet Core Demo, C++" << endl << endl;;
+    cout << "Wallet Core Demo, C++" << endl << endl;
+    cout << "DISCLAIMER: This is a sample application with demonstration purposes only, do not use it with real addresses, or real transactions, use it at your own risk." << endl;
 
+    // Create a new wallet, with new mnemonic passphrase
     cout << "Creating a new HD wallet ... ";
     TWHDWallet* wallet = TWHDWalletCreate(128, TWStringCreateWithUTF8Bytes(""));
     cout << "done." << endl;
@@ -24,6 +28,7 @@ int main()
         TWStringUTF8Bytes(TWCoinTypeConfigurationGetName(coinType)) << " " <<
         TWStringUTF8Bytes(TWCoinTypeConfigurationGetSymbol(coinType)) << endl; 
 
+    // Derive default address.  Done in 2 steps: derive private key, then address from private key
     cout << "Obtaining default address ... "; 
     TWPrivateKey* secretPrivateKey = TWHDWalletGetKeyForCoin(wallet, coinType);
     string address = TWStringUTF8Bytes(TWCoinTypeDeriveAddress(coinType, secretPrivateKey));
@@ -31,6 +36,21 @@ int main()
 
     cout << "RECEIVE funds: send to the address:   " << address << " ." << endl << endl;
 
+    // Steps for sending:
+    // - put together a send message (contains sender and receiver address, amount, etc.)
+    // - sign this message
+    // - broadcast this message to the P2P network -- not done in this sample
     cout << "SEND funds:" << endl;
+    cout << "preparing transaction ... ";
+    const string dummyReceiverAddress = "0xC37054b3b48C3317082E7ba872d7753D13da4986";
+    //string transaction = R"({"chainId":"AQ==","gasPrice":"1pOkAA==","gasLimit":"Ugg=","toAddress":")";
+    //transaction += dummyReceiverAddress;
+    //transaction += R"(","amount":"A0i8paFgAA=="})";
+    //cout << transaction << endl;
+    
+    cout << "signing transaction ... ";
+    
+    cout << "SEND funds:" << endl;
+
     cout << "  TODO:" << endl;
 }
